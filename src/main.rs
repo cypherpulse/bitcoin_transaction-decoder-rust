6f344cdcf1a72736995c634b44b0f57faadf33fc
cypherpulse <johnbradill67@gmail.com>
2026-05-29 06:28:55 +0300

    Fix version extraction logic in read_version function

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@
 
 fn read_version(transaction_hex: &str)-> u32 {
     let transaction_bytes = hex::decode(transaction_hex).unwrap();
-    let version_bytes = &transaction_bytes[0..4];
-    println!("version bytes: {:?}", version_bytes);
+    let version_bytes =<[u8; 4]>::try_from( &transaction_bytes[0..4]);
 
+    let version = u32::from_le_bytes(version_bytes);
+    println!("version bytes: {:?}", version_bytes);
     1
 }
 

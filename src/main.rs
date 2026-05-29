8abd6f3e6013437a2a7face5097e9aebdc52c07d
cypherpulse <johnbradill67@gmail.com>
2026-05-29 07:35:35 +0300

    Refactor read_version function to use a buffer for reading version bytes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-#![allow(unused)]
+use std::io::Read;
+
+#[allow(unused)]
 
 
 fn read_version(transaction_hex: &str)-> u32 {
@@ -11,13 +13,20 @@ fn read_version(transaction_hex: &str)-> u32 {
 
     //TryInto/Arrays/Slices- here we are using the try_into method to convert a slice of bytes (the first 4 bytes of the transaction) into an array of 4 bytes. The try_into method is a more concise way to perform this conversion compared to using the try_from function. It also returns a Result type, which we unwrap to get the array of bytes.
     //TryInto is a trait that provides a way to convert between types. In this case, we are using it to convert a slice of bytes into an array of bytes. The try_into method is called on the slice of bytes, and it attempts to convert it into an array of 4 bytes. If the conversion is successful, it returns Ok with the array; if it fails, it returns Err. We unwrap the Result to get the array of bytes directly.
-    let version_bytes: [u8; 4] = (&transaction_bytes[0..4]).try_into().unwrap();
+    // let version_bytes: [u8; 4] = (&transaction_bytes[0..4]).try_into().unwrap();
 
-   u32::from_le_bytes(version_bytes)
+    let transaction_bytes = hex::decode(transaction_hex).unwrap();
+
+    let mut bytes_slice = transaction_bytes.as_slice();
+    let mut buffer=[0; 4];
+    bytes_slice.read(&mut buffer).unwrap();
+
+
+   u32::from_le_bytes(buffer)
 }
 
 fn main() {
-    let version = read_version("000000000001c47896df6c74aa8351f371feef54d0b9a90516d74ebe4d0d828");
+    let version = read_version("010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000");
     println!("version: {}",version);
    
 }

e9894104f8cc3e78960d398f755ace6257c393be
cypherpulse <johnbradill67@gmail.com>
2026-06-02 03:45:23 +0300

    test: add additional test cases for read_compact_size function

--- src/main.rs
+++ src/main.rs
@@ -72,5 +72,17 @@ mod test {
         let mut bytes = [1_u8].as_slice();
         let count=read_compact_size(&mut bytes);
         assert_eq!(count, 1_u64);
+
+        let mut bytes =[253_u8, 0, 1].as_slice();
+        let count=read_compact_size(&mut bytes);
+        assert_eq!(count, 256_u64);
+
+        let mut bytes =[254_u8, 0, 0, 0, 1].as_slice();
+        let count=read_compact_size(&mut bytes);
+        assert_eq!(count, 256_u64.pow(3));
+
+        let mut bytes =[255_u8, 0, 0, 0, 0, 0, 0, 0, 1].as_slice();
+        let count=read_compact_size(&mut bytes);
+        assert_eq!(count, 256_u64.pow(7));
     }
 }

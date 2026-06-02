374e182143dabcb6139fc95e40c98ba20389e0a6
cypherpulse <johnbradill67@gmail.com>
2026-06-02 03:48:26 +0300

    test: add assertion for read_compact_size function in unit tests

--- src/main.rs
+++ src/main.rs
@@ -84,5 +84,12 @@ mod test {
         let mut bytes =[255_u8, 0, 0, 0, 0, 0, 0, 0, 1].as_slice();
         let count=read_compact_size(&mut bytes);
         assert_eq!(count, 256_u64.pow(7));
+
+        let hex = "fd204e";
+        let decode = hex::decode(hex).unwrap();
+        let mut bytes = decode.as_slice();
+        let count=read_compact_size(&mut bytes);
+        let expected_count = 20_000_u64;
+        assert_eq!(count, expected_count);
     }
 }

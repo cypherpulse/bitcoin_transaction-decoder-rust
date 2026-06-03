1778a0af2449441d5c006f6a7bd61db8bb32e757
cypherpulse <johnbradill67@gmail.com>
2026-06-03 14:54:58 +0300

    feat: update Transaction and Input structs to use String for txid and script_sig

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ use std::io::Read;
 use serde::{Serialize};
 
 
+#[derive(Debug,Serialize)]
+struct Transaction {
+    version: u32,
+    inputs: Vec<Input>,
+}
 #[allow(unused)]
+#[derive(Debug,Serialize)]
 
-&[derive(Debug,Serialize)]
 
 struct Input{
-    txid: [u8; 32],
+    txid: String,
     output_index: u32,
-    script_sig: Vec<u8>,
+    script_sig: String,
     sequence: u32,
 }
 
@@ -74,17 +79,18 @@ fn read_u32(transaction_bytes: &mut &[u8])-> u32 {
     u32::from_le_bytes(buffer)
 }
 
-fn read_txid(transaction_bytes: &mut &[u8])-> [u8; 32] {
+fn read_txid(transaction_bytes: &mut &[u8])-> String {
     let mut buffer = [0; 32];
     transaction_bytes.read(&mut buffer).unwrap();
-    buffer
+    buffer.reverse();
+    hex::encode(buffer)
 }
 
-fn read_script(transaction_bytes: &mut &[u8])->Vec<u8> {
+fn read_script(transaction_bytes: &mut &[u8])-> String {
     let script_size = read_compact_size(transaction_bytes) as usize;
     let mut buffer = vec![0_u8; script_size];
     transaction_bytes.read(&mut buffer).unwrap();
-    buffer
+    hex::encode(buffer)
 }
 fn main() {
     let transaction_hex = "010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf72587";
@@ -107,10 +113,17 @@ fn main() {
             sequence,
         });
     }
-    let json_inputs = serde_json::to_string_pretty(&inputs).unwrap();
-    println!("bytes slice first element: {:?}", bytes_slice.first());
-    println!("version: {}", version);
-    println!("input count: {}", input_count);
+    
+    let transaction = Transaction {
+        version,
+        inputs,
+    };
+
+    println!("transaction: {}", serde_json::to_string_pretty(&transaction).unwrap());
+    // let json_inputs = serde_json::to_string_pretty(&inputs).unwrap();
+    // println!("bytes slice first element: {:?}", bytes_slice.first());
+    // println!("version: {}", version);
+    // println!("inputs: {}", json_inputs);
 }
 
 #[cfg(test)]
